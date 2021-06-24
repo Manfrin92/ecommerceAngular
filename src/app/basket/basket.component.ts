458ef4a8c7f4532736094acc59b2198b940a1f34
manfrin92 <manfredd_10@hotmail.com>
2021-06-24 20:15:23 -0300

    feat: table for displaying basket

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -4,16 +4,16 @@ import { IBasket } from '../shared/models/basket';
 import { BasketService } from './basket.service';
 
 @Component({
-    selector: 'app-basket',
-    templateUrl: './basket.component.html',
-    styleUrls: ['./basket.component.scss'],
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent implements OnInit {
-    basket$: Observable<IBasket>;
+  basket$: Observable<IBasket>;
 
-    constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService) {}
 
-    ngOnInit(): void {
-        this.basket$ = this.basketService.basket$;
-    }
+  ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
+  }
 }
