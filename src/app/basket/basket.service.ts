6a234093ccd8c82547f3e5d16c674e50b4e42d3b
manfrin92 <manfredd_10@hotmail.com>
2021-07-12 17:52:42 -0300

    feat: add/remove item to basket function

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -63,6 +63,51 @@ export class BasketService {
     this.setBasket(basket);
   }
 
+  incrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((x) => x.id === item.id);
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((x) => x.id === item.id);
+    if (basket.items[foundItemIndex].quantity > 1) {
+      basket.items[foundItemIndex].quantity--;
+      this.setBasket(basket);
+    } else {
+      this.removeItemFromBasket(item);
+    }
+  }
+  removeItemFromBasket(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    if (basket.items.some((x) => x.id === item.id)) {
+      basket.items = basket.items.filter((i) => i.id !== item.id);
+      if (basket.items.length > 0) {
+        this.setBasket(basket);
+      } else {
+        this.deleteBasket(basket);
+      }
+    }
+  }
+  deleteBasket(basket: IBasket) {
+    return this.http
+      .delete(this.baseUrl + `basketId?id=${basket.id}`)
+      .subscribe(
+        () => {
+          // @ts-ignore
+          this.basketSource.next(null);
+          // @ts-ignore
+          this.basketTotalSource.next(null);
+          localStorage.removeItem('basket_id');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
   private addOrUpdateItem(
     items: IBasketItem[],
     itemToAdd: IBasketItem,
