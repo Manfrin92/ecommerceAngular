69663b39a9a20a20f20a1dfd4054657d851917ec
manfrin92 <manfredd_10@hotmail.com>
2021-07-12 18:03:45 -0300

    feat: product-detail add or remove item

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,40 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
-    selector: 'app-product-details',
-    templateUrl: './product-details.component.html',
-    styleUrls: ['./product-details.component.scss'],
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
-    product: IProduct;
+  product: IProduct;
+  quantity = 1;
 
-    constructor(
-        private shopService: ShopService,
-        private activetedRoute: ActivatedRoute,
-        private bcService: BreadcrumbService
-    ) {
-        this.bcService.set('@productDetails', ' ');
-    }
+  constructor(
+    private shopService: ShopService,
+    private activetedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
-    ngOnInit(): void {
-        this.loadProduct();
-    }
+  ngOnInit(): void {
+    this.loadProduct();
+  }
 
-    loadProduct() {
-        this.shopService
-            .getProduct(Number(this.activetedRoute.snapshot.paramMap.get('id')))
-            .subscribe(
-                (product) => {
-                    this.product = product;
-                    this.bcService.set('@productDetails', product.name);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 0) {
+      this.quantity--;
     }
+  }
+
+  loadProduct() {
+    this.shopService
+      .getProduct(Number(this.activetedRoute.snapshot.paramMap.get('id')))
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
